5fe28d7e0de6055515ca40cbc1da44410397e53c
markroche32 <markroche32@gmail.com>
2018-09-18 22:47:48 +0100

    Added sampleposts.component to display all travel posts in database once navigated

--- src/app/components/samplepost/samplepost.component.ts
+++ src/app/components/samplepost/samplepost.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../user';
+import { Post } from '../../post';
+import { LoaderService } from '../../services/loader.service';
+import { PostsService } from '../../services/posts.service';
+import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-samplepost',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SamplepostComponent implements OnInit {
 
-  constructor() { }
+  myPosts : Post[];
+  user : User;
+  imageURL : string;
+
+  constructor(
+    private loaderService : LoaderService, 
+    private postService: PostsService, 
+    private router: Router) { }
 
   ngOnInit() {
+
+    this.loaderService.showOverlay(true);
+    
+        console.log("dev environment api url  " + environment.apiUrl);
+        
+        this.imageURL = environment.apiUrl;
+    
+        this.postService.getPosts().subscribe(
+          posts => {
+            this.myPosts = posts;
+            this.loaderService.showOverlay(false);
+            }, 
+          error => {
+            console.log(error);
+            this.loaderService.showOverlay(false);
+          });
+
   }
 
 }
